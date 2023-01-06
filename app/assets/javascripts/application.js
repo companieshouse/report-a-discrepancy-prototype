d3748eca1f32983a392bc62851d8aaa5d228316d
mtomas-ch <119611289+mtomas-ch@users.noreply.github.com>
2023-01-06 13:49:56 +0000

    Added loop for discrepancy types

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,3 +6,8 @@
 window.GOVUKPrototypeKit.documentReady(() => {
   // Add JavaScript here
 })
+
+
+ 
+
+
--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,26 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+// Run this code when a form is submitted to 'juggling-balls-answer'
+router.post('/update-discrepancy-type', function (req, res) {
+    // Set number of checkboxes checked
+    var num_checks = (req.session.data['psc-options']).length;
+
+    // Set count to one more
+    var count = parseInt(req.session.data['discrepancy-count']) +1;
+    req.session.data['discrepancy-count'] = count.toString();
+
+    // Get the next checkbox in the list
+    var discrepancy_type = req.session.data['psc-options'][count]
+    req.session.data['disc-type'] = discrepancy_type;
+
+    if (count == num_checks) {
+        // next page
+        res.redirect('/v1/discrepancy-details')
+     } else {
+        // reload page with updated type
+        res.redirect('/v1/discrepancy-catagory')
+     }
+  
+})
